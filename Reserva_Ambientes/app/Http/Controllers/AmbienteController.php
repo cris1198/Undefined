44b77bc562afd09aea4b8d4001873c2b0dd8c26f
dachv1999 <dachv1999@gmail.com>
2022-04-25 17:05:45 -0400

    Update terminado

--- Reserva_Ambientes/app/Http/Controllers/AmbienteController.php
+++ Reserva_Ambientes/app/Http/Controllers/AmbienteController.php
@@ -22,7 +22,6 @@ class AmbienteController extends Controller
         $nuevaAula->nombreAula = $request->nombreAula; 
         $nuevaAula->ubicacion = $request->ubicacion;
         $nuevaAula->save();
-        $variablequenotieneimportancia=0;
     }
     public function getById($id)               //retorna un Ambiente por el ID
     {
@@ -30,33 +29,30 @@ class AmbienteController extends Controller
         return $ambiente;                      //JSON con los ambientes
     }
 
-    public function search(Request $request)   //busca codigo, nombreAula o ubicacion
+    public function search_and_filter(Request $request)      //busca codigo, nombreAula o ubicacion, mas los filtros 
     {
-        $buscar = $request->input('buscar');   //busca variables de el url buscar=palabra_buscada
-        $ambientes = Aula::search($buscar);    //devuelve coleccion de Aulas
-        return $ambientes;                     //devuelve JSON con datos de Aula
+        $buscar = $request->buscar;                          //obteniendo los datos de JSON
+        $caracteristicas = $request->caracteristicas;
+        $tipo = $request->tipo;
+        $rangoBajo = $request->rangoBajo;
+        $rangoAlto = $request->rangoAlto;
+
+        $ambientes = Aula::search($buscar,                  
+        $caracteristicas, $tipo, $rangoBajo, $rangoAlto);    //devuelve coleccion de Aulas
+        return $ambientes;                                   //devuelve JSON con datos de Aula
     }
 
-    public function filter(Request $request)    //busca tipo o caracteristicas
+    public function update(Request $request, $id)        //actualiza los datos de un ambiente
     {
-        $filtrar = $request->input('filtro');   //busca variables de el url filtro=palabra_filtrada
-        $ambientes = Aula::filter($filtrar);    //devuelve coleccion de Aulas
-        return $ambientes;                      //devuelve JSON con datos de Aula
-    }
-
-    public function rangeFilter(Request $request)              //busca un rango de capacidad 
-    {
-        $rangeDown = $request->input('rango_Bajo');            //busca variables de el url rango_Alto=Numero_Bajo
-        $rangeUp = $request->input('rango_Alto');              //busca variables de el url rango_Alto=Numero_Alto   
-        $ambientes = Aula::rangeFilter($rangeUp, $rangeDown);  //devuelve coleccion de Aulas en el rango de capacidad
-        return $ambientes;                                     //devuelve JSON con datos de Aula
-    }
-
-    public function update($id)               //actualiza los datos de un ambiente
-    {
-        Aula::findOrFail($id);                //si no encuentra ambiente devuelve falso
-        $ambientes = Aula::all();
-        return response()->json([              //JSON con los ambientes
+        $aula = Aula::findOrFail($id);                   //si no encuentra ambiente devuelve falso
+        $aula->capacidad = $request->capacidad;
+        $aula->codigo = $request->codigo;  
+        $aula->tipo = $request->tipo;  
+        $aula->caracteristicas = $request->caracteristicas;
+        $aula->nombreAula = $request->nombreAula; 
+        $aula->ubicacion = $request->ubicacion;
+        $aula->save();                                   //guarda cambios
+        return response()->json([                        //JSON con los ambientes
             'Respuesta' => 'Actualizado correctamente'
         ], 202);                   
     }
